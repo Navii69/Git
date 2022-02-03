a63633f9a031c9b609c70617cfbbb9d3b7323650
Navii-Rocks <navigameid@gmail.com>
2022-02-03 19:07:30 +0530

    snake can get the apple and the game also gets over

--- snake_game/main.py
+++ snake_game/main.py
@@ -1,11 +1,42 @@
-from re import I
-import pygame, random
+import pygame, random , sys
 from pygame.math import Vector2
 
+class MAIN:
+    def __init__(self):
+        self.snake = SNAKE()
+        self.fruit = FRUIT()
+
+    def update(self):
+        self.snake.move_snake()
+        self.check_collision()
+        self.check_fail()
+
+    def draw_elements(self):
+        self.fruit.draw_fruit()
+        self.snake.draw_snake()
+    
+    def check_collision(self):
+        if self.fruit.pos == self.snake.body[0]:
+            self.fruit.randomize()
+            self.snake.add_block()
+    
+    def check_fail(self):
+        if not 0<= self.snake.body[0].x < cell_number:
+            self.game_over()
+        
+        elif not 0 <= self.snake.body[0].y < cell_number:
+            self.game_over()
+
+        for block in self.snake.body[1:]:
+            if block == self.snake.body[0]:
+                self.game_over()
+    def game_over(self):
+        sys.exit()
 class SNAKE:
     def __init__(self):
-        self.body = [Vector2(5,10), Vector2(6,10), Vector2(7,10)]
+        self.body = [Vector2(5,10), Vector2(4,10), Vector2(3,10)]
         self.direction = Vector2(1,0)
+        self.new_block = False
 
     def draw_snake(self):
         for block in self.body:
@@ -15,9 +46,19 @@ class SNAKE:
             pygame.draw.rect(screen,(13,111,122),block_rect)
     
     def move_snake(self):
-        body_copy = self.body[:-1]
-        body_copy.insert(0, body_copy[0] + self.direction)
-        self.body = body_copy[:]
+        if self.new_block == True:
+            body_copy = self.body[:]
+            body_copy.insert(0, body_copy[0] + self.direction)
+            self.body = body_copy[:]
+            self.new_block = False
+        else:
+            body_copy = self.body[:-1]
+            body_copy.insert(0, body_copy[0] + self.direction)
+            self.body = body_copy[:]
+    
+    def add_block(self):
+        self.new_block = True
+
 
 class FRUIT:
     def __init__(self):
@@ -29,6 +70,11 @@ class FRUIT:
         fruit_rect = pygame.Rect(self.pos.x * cell_size,self.pos.y * cell_size,cell_size,cell_size)
         pygame.draw.rect(screen,(126,166,114),fruit_rect)
 
+    def randomize(self):
+        self.x = random.randint(0, cell_number - 1 ) # x pos
+        self.y = random.randint(0, cell_number - 1) # y pos
+        self.pos = Vector2(self.x,self.y)
+
 if __name__ == "__main__":# __name__ = file name
     pygame.init() #Doesnt work without this line
 
@@ -38,35 +84,34 @@ cell_number = 20
 screen = pygame.display.set_mode((cell_size * cell_number,cell_size * cell_number))
 clock = pygame.time.Clock()# helps in fps lock(so game speed is same in every pc)
 
-running = True
-
-fruit= FRUIT()
-snake= SNAKE()
-
 SCREEN_UPDATE = pygame.USEREVENT
 pygame.time.set_timer(SCREEN_UPDATE,150)# 150 is in milliseconds
 
+main_game = MAIN()
 # Game Loop
-while running ==  True :
+while True :
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
+            sys.exit()
         
         if event.type == SCREEN_UPDATE:
-            snake.move_snake()
+            main_game.update()
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP:
-                snake.direction = Vector2(0,-1)
+                if main_game.snake.direction.y !=1:
+                    main_game.snake.direction = Vector2(0,-1)
             elif event.key == pygame.K_DOWN:
-                snake.direction = Vector2(0,1)
+                if main_game.snake.direction.y !=-1:
+                    main_game.snake.direction = Vector2(0,1)
             elif event.key == pygame.K_LEFT:
-                snake.direction = Vector2(-1,0)
+                if main_game.snake.direction.x !=1:
+                    main_game.snake.direction = Vector2(-1,0)
             elif event.key == pygame.K_RIGHT:
-                snake.direction = Vector2(1,0)
+                if main_game.snake.direction.x !=-1:
+                    main_game.snake.direction = Vector2(1,0)
             
     screen.fill((pygame.Color("gold")))# Alternative pygame.color()(inside the rgb tuple) to use predefined colours
-    fruit.draw_fruit()
-    snake.draw_snake()
+    main_game.draw_elements()
     pygame.display.update()
     clock.tick(60)# limited the fps to 60 frames
