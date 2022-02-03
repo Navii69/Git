ebd84c3843fe0b88f0691c0f65987c830475015c
Navii-Rocks <navigameid@gmail.com>
2022-02-03 18:17:31 +0530

    got the snake moving boi

--- snake_game/main.py
+++ snake_game/main.py
@@ -1,9 +1,11 @@
+from re import I
 import pygame, random
 from pygame.math import Vector2
 
 class SNAKE:
     def __init__(self):
         self.body = [Vector2(5,10), Vector2(6,10), Vector2(7,10)]
+        self.direction = Vector2(1,0)
 
     def draw_snake(self):
         for block in self.body:
@@ -11,6 +13,12 @@ class SNAKE:
             y_pos = int(block.y) * cell_size
             block_rect = pygame.Rect(x_pos, y_pos,cell_size,cell_size)
             pygame.draw.rect(screen,(13,111,122),block_rect)
+    
+    def move_snake(self):
+        body_copy = self.body[:-1]
+        body_copy.insert(0, body_copy[0] + self.direction)
+        self.body = body_copy[:]
+
 class FRUIT:
     def __init__(self):
         self.x = random.randint(0, cell_number - 1 ) # x pos
@@ -28,20 +36,37 @@ if __name__ == "__main__":# __name__ = file name
 cell_size = 40
 cell_number = 20
 screen = pygame.display.set_mode((cell_size * cell_number,cell_size * cell_number))
-screen.fill((pygame.Color("gold")))# Alternative pygame.color()(inside the rgb tuple) to use predefined colours
 clock = pygame.time.Clock()# helps in fps lock(so game speed is same in every pc)
 
 running = True
 
 fruit= FRUIT()
 snake= SNAKE()
+
+SCREEN_UPDATE = pygame.USEREVENT
+pygame.time.set_timer(SCREEN_UPDATE,150)# 150 is in milliseconds
+
 # Game Loop
 while running ==  True :
-    fruit.draw_fruit()
-    snake.draw_snake()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    
+        
+        if event.type == SCREEN_UPDATE:
+            snake.move_snake()
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                snake.direction = Vector2(0,-1)
+            elif event.key == pygame.K_DOWN:
+                snake.direction = Vector2(0,1)
+            elif event.key == pygame.K_LEFT:
+                snake.direction = Vector2(-1,0)
+            elif event.key == pygame.K_RIGHT:
+                snake.direction = Vector2(1,0)
+            
+    screen.fill((pygame.Color("gold")))# Alternative pygame.color()(inside the rgb tuple) to use predefined colours
+    fruit.draw_fruit()
+    snake.draw_snake()
     pygame.display.update()
     clock.tick(60)# limited the fps to 60 frames
