d4ea808210a1fa0e079df6ef1d9ccfbdc516c462
Navii-Rocks <navigameid@gmail.com>
2022-02-04 18:22:37 +0530

    added snake block

---
+++ pacman/main.py
@@ -0,0 +1,14 @@
+import pygame, sys
+
+pygame.init()
+
+cell_size = 50
+cell_number = 20
+
+screen = pygame.display.set_mode(cell_size * cell_number , cell_size * cell_number)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT():
+            sys.exit()
+    
