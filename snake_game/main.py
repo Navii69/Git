15d5cf6b9812a5d9f5bfcda687408528c23d38bb
Navii-Rocks <navigameid@gmail.com>
2022-02-04 10:59:07 +0530

    made the board

--- snake_game/main.py
+++ snake_game/main.py
@@ -12,6 +12,7 @@ class MAIN:
         self.check_fail()
 
     def draw_elements(self):
+        self.draw_grass()
         self.fruit.draw_fruit()
         self.snake.draw_snake()
     
@@ -32,7 +33,22 @@ class MAIN:
                 self.game_over()
     def game_over(self):
         sys.exit()
-        
+
+    def draw_grass(self):
+        grass_color = (167,209,61)
+
+        for row in range(cell_number):
+            if row%2 == 0:
+                for col in range(cell_number):
+                    if col%2 == 0:
+                        grass_rect = pygame.Rect(col * cell_size,row * cell_size,cell_size,cell_size)
+                        pygame.draw.rect(screen,grass_color,grass_rect)
+            else:
+                for col in range(cell_number):
+                    if col%2 != 0:
+                        grass_rect = pygame.Rect(col * cell_size,row * cell_size,cell_size,cell_size)
+                        pygame.draw.rect(screen,grass_color,grass_rect)
+                        
 class SNAKE:
     def __init__(self):
         self.body = [Vector2(5,10), Vector2(4,10), Vector2(3,10)]
