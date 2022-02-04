73e2928fe29c9629636650743e41d71c865bef42
Navii-Rocks <navigameid@gmail.com>
2022-02-04 11:19:51 +0530

    added scoreboard

--- snake_game/main.py
+++ snake_game/main.py
@@ -15,7 +15,8 @@ class MAIN:
         self.draw_grass()
         self.fruit.draw_fruit()
         self.snake.draw_snake()
-    
+        self.draw_score()
+
     def check_collision(self):
         if self.fruit.pos == self.snake.body[0]:
             self.fruit.randomize()
@@ -48,7 +49,21 @@ class MAIN:
                     if col%2 != 0:
                         grass_rect = pygame.Rect(col * cell_size,row * cell_size,cell_size,cell_size)
                         pygame.draw.rect(screen,grass_color,grass_rect)
-                        
+
+    def draw_score(self):
+        score_text = str(len(self.snake.body) - 3)
+        score_surface = game_font.render(score_text, True, (56, 74, 12))
+        score_x = int(cell_size * cell_number - 60)
+        score_y = int(cell_size * cell_number - 40)
+        score_rect = score_surface.get_rect(center = (score_x,score_y))
+        apple_rect = apple.get_rect(midright = (score_rect.left, score_rect.centery))
+        bg_rect = pygame.Rect(apple_rect.left, apple_rect.top, apple_rect.width + score_rect.width + 6, apple_rect.height)
+
+        pygame.draw.rect(screen,(167,209,61), bg_rect)
+        screen.blit(score_surface,score_rect)
+        screen.blit(apple, apple_rect)
+        pygame.draw.rect(screen,(56,74,12), bg_rect,2)
+
 class SNAKE:
     def __init__(self):
         self.body = [Vector2(5,10), Vector2(4,10), Vector2(3,10)]
@@ -101,6 +116,8 @@ cell_number = 20
 screen = pygame.display.set_mode((cell_size * cell_number,cell_size * cell_number))
 clock = pygame.time.Clock()# helps in fps lock(so game speed is same in every pc)
 apple = pygame.image.load("snake_game/resources/apple.png").convert_alpha()
+game_font = pygame.font.Font("snake_game/resources/Dancing Minotaur.ttf",25)
+
 SCREEN_UPDATE = pygame.USEREVENT
 pygame.time.set_timer(SCREEN_UPDATE,150)# 150 is in milliseconds
 
